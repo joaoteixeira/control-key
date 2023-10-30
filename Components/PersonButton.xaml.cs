2cf9b972bca661a0cea67c7c14f03329770802cd
João Teixeira <joao.teixeira@ifro.edu.br>
2023-10-30 17:09:47 -0400

    add user control

---
+++ Components/PersonButton.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+
+namespace ControlKeys.Components
+{
+    /// <summary>
+    /// Interação lógica para PersonButtonItem.xam
+    /// </summary>
+    public partial class PersonButton : UserControl
+    {
+        public string Title { get; set; }
+        public string Subtitle { get; set; }
+
+        public PersonButton()
+        {
+            InitializeComponent();
+            this.DataContext = this;
+        }
+
+        public event RoutedEventHandler Click
+        {
+            add { B1.AddHandler(ButtonBase.ClickEvent, value); }
+            remove { B1.RemoveHandler(ButtonBase.ClickEvent, value); }
+        }
+    }
+}
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,10 +1,12 @@
-﻿using System;
+﻿using ControlKeys.Components;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -23,6 +25,23 @@ namespace ControlKeys
         public MainWindow()
         {
             InitializeComponent();
+            Loaded += MainWindow_Loaded;
+        }
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            var p1 = new PersonButton() { Title = "João Junior", Subtitle = "TAE" };
+            var p2 = new PersonButton() { Title = "João Teixeira", Subtitle = "Docente" };
+            p1.Click += Button_Click;
+            p2.Click += Button_Click;
+
+            WPanel.Children.Add(p1);
+            WPanel.Children.Add(p2);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Teste");
         }
     }
 }
